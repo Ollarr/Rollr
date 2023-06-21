6751f31b273b00361ea2aea245ab5d62a16f0d55
Oluwasegun <oluyedis@gmail.com>
2023-06-21 13:38:41 +0100

    created a custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,11 @@
 import "package:flutter/material.dart";
+import "package:rollr/gradient_container.dart";
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 85, 3, 103),
-              Color.fromARGB(255, 15, 47, 211)
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-          ),
-          child: const Center(
-              child: Text(
-            'Hello World!',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 22,
-            ),
-          )),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
